ed8ad2e54fa4cd8dbd4bc3d2cdae1ed063742582
Diogo <diogokasteller@gmail.com>
2022-08-25 16:16:37 -0300

    Chrome Extension Update

--- 03 - Chrome Extension/scripts/index.js
+++ 03 - Chrome Extension/scripts/index.js
@@ -1,19 +1,55 @@
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const tabBtn = document.getElementById('tab-btn')
+const deleteBtn = document.getElementById('delete-btn')
+const ulEl = document.getElementById('ul-el')
 let myLeads = []
+let oldLeads = []
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value)
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  inputEl.value = ''
+  render(myLeads)
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i=0; i < myLeads.length; i++) {
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-    }
+tabBtn.addEventListener('click', function () {
+  // Grab the URL of the current tab
+  tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    let activeTab = tabs[0]
+    let activeTabId = activeTab.id
+  })
+  myLeads.push(tabs[0].url)
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
 
-    ulEl.innerHTML = listItems
+deleteBtn.addEventListener('click', function () {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+function render(leads) {
+  let listItems = ''
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]} 
+                </a>
+            </li>
+        `
+  }
+
+  ulEl.innerHTML = listItems
 }
